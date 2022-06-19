8c070e69689f31bc9e2d315ecca763cd456eb5aa
langnang <langnang.chen@outlook.com>
2022-06-19 12:01:43 +0800

    router

---
+++ router/index.js
@@ -0,0 +1,5 @@
+const router = [
+  { name: "密", path: "./views/crypto", meta: { dependencies: [] }, },
+  { name: "表", path: "./views/xlsx", meta: { dependencies: [] }, },
+  { name: "JSON", path: "./views/xlsx", span: 2, meta: { dependencies: [], }, },
+]
