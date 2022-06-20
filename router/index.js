cafd857fadf04224ceb778b92525b07c7d4d17a0
langnang <langnang.chen@outlook.com>
2022-06-20 15:52:54 +0800

    toolkit

--- router/index.js
+++ router/index.js
@@ -1,6 +1,6 @@
 const router = [
-  { name: "加密", path: "./views/crypto/index.html", meta: { dependencies: [] }, },
-  { name: "表格", path: "./views/xlsx/index.html", meta: { dependencies: [] }, },
-  { name: "图标", path: "./views/icon/index.html", meta: { dependencies: [] }, },
+  { name: "Crypto", path: "./views/crypto/index.html", meta: { dependencies: [] }, },
+  { name: "XLSX", path: "./views/xlsx/index.html", meta: { dependencies: [] }, },
+  { name: "ICON", path: "./views/icon/index.html", meta: { dependencies: [] }, },
   { name: "JSON", path: "./views/xlsx/index.html", meta: { dependencies: [], }, },
 ]
